afc21db27c012b487c7d01e0cf444f76f30d8398
JWinawer <jonathan.winawer@nyu.edu>
2018-04-26 18:10:07 -0400

    cleanup

--- setup.m
+++ setup.m
@@ -4,12 +4,17 @@
 % Download the dataset if it has not already been downloaded.
 % (If you like, you may manually download the dataset from:
 %    http://kendrickkay.net/analyzePRF/exampledataset.mat)
-files = {'exampledataset.mat'};
+files = {'exampledataset.mat' 'socmodel.zip'};
+source = {'http://kendrickkay.net/analyzePRF/exampledataset.mat' ...
+    'http://kendrickkay.net/socmodel/socmodel.zip'};
+
 for p=1:length(files)
   if ~exist(files{p},'file')
     fprintf('Downloading %s (please be patient).\n',files{p});
-    urlwrite(sprintf('http://kendrickkay.net/analyzePRF/%s',files{p}),files{p});
+    urlwrite(source{p},files{p});
     fprintf('Downloading is done!\n');
+    [~, ext] = fileparts(files{p});
+    if strcmp(ext, 'zip'), unzip(files{p});  end
   end
 end
 clear p files;
